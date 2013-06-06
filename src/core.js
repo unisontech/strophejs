c29c4aa1c47326debdadeacf91d8fdbddc313d4f
Dmitry Korlas <dkorlas@unison.com>
2013-06-06 19:25:30 +0400

    Inject commit from UWA dac3417	2012.09.21 do not escape special symbols from text in xmpp message

--- src/core.js
+++ src/core.js
@@ -486,8 +486,8 @@ Strophe = {
      */
     xmlTextNode: function (text)
     {
-	//ensure text is escaped
-	text = Strophe.xmlescape(text);
+    	//ensure text is escaped
+    	// text = Strophe.xmlescape(text);
 
         return Strophe.xmlGenerator().createTextNode(text);
     },
